99909f12c9553302b9f18e1d865ed43a110ac9fc
Peter Meth <pmeth@rogers.com>
2012-04-06 00:07:47 -0400

    reformatting the student file.

--- student/123/index.php
+++ student/123/index.php
@@ -5,21 +5,19 @@
 	<body>
 		<div style="border: 1px solid red;">
 
-<?php
+			<?php
+			if (!empty($_POST['submit'])) {
+				echo "You submitted " . $_POST['yourname'] . "<br>";
+				echo "<a href='.'>Start Over</a>";
+				exit;
+			}
+			?>
 
-if(!empty($_POST['submit'])) {
-	echo "You submitted " . $_POST['yourname'] . "<br>";
-	echo "<a href='.'>Start Over</a>";
-	exit;
-}
-
-?>
-
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-	Your Name: <input type="text" name="yourname" />
-	<br>
-	<input type="submit" name="submit" value="RSVP" />
-</form>
-</div>
-</body>
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+				Your Name: <input type="text" name="yourname" />
+				<br>
+				<input type="submit" name="submit" value="RSVP" />
+			</form>
+		</div>
+	</body>
 </html>
