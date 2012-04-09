086371fa57acffdb28973d62ef7d5b1924b642e8
Peter Meth <pmeth@rogers.com>
2012-04-09 16:47:03 -0400

    trying to jam the ide into the jquery layout.

--- js/layout.js
+++ js/layout.js
@@ -2,11 +2,8 @@ var myLayout; // a var is required because this page utilizes: myLayout.allowOve
 
 $(document).ready(function () {
 	myLayout = $('body').layout({
-		south: {
-			initHidden: true
-		},
 		west: {
-			size: 200
+			size: 600
 		},
 		east: {
 			size: 600
