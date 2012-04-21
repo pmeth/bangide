7a2e12f3413cbebe7c07c8e16417f5e203ba7518
Peter Meth <pmeth@rogers.com>
2012-04-20 23:32:08 -0400

    trying to fix a bug when image is loaded in the center pane

--- js/simple.js
+++ js/simple.js
@@ -13,16 +13,17 @@ $(document).ready(function () {
 		}
 	});
 
-	editor = CodeMirror.fromTextArea(document.getElementById("tabs1-code"), {
-		lineNumbers: true,
-		matchBrackets: true,
-		mode: "application/x-httpd-php",
-		indentWithTabs: true,
-		enterMode: "keep",
-		tabMode: "shift",
-		autofocus: true
-	});
-
+	if(document.getElementById("tabs1-code")) {
+		editor = CodeMirror.fromTextArea(document.getElementById("tabs1-code"), {
+			lineNumbers: true,
+			matchBrackets: true,
+			mode: "application/x-httpd-php",
+			indentWithTabs: true,
+			enterMode: "keep",
+			tabMode: "shift",
+			autofocus: true
+		});
+	}
 	$('#filetree').treeview({
 		collapsed: true
 	});
