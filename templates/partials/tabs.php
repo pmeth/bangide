f869b8ce33bc7e45b920d2e26fcffb88b1c6441f
petermeth <petermeth@192.168.0.190>
2012-04-22 15:58:20 -0400

    switched the order of "after save" actions

--- templates/partials/tabs.php
+++ templates/partials/tabs.php
@@ -7,20 +7,21 @@
  */
 class tabs {
 
-	static function render(array $openfiles, $filetreepath, $projectpath) {
-		$links = "<ul>";
-		$divs = "";
-		foreach ($openfiles as $counter => $filename) {
-			$links .= "<li><a href='#tabs-$counter'>$filename</a> <a href='#' id='close-tabs-$counter' class='close-tabs'>x</a></li>";
-			$content = file_get_contents($filetreepath . $filename);
-			$divs .= "
+    static function render(array $openfiles, $filetreepath, $projectpath) {
+        $links = "<ul>";
+        $divs = "";
+        foreach ($openfiles as $counter => $filename) {
+            $links .= "<li><a href='#tabs-$counter'>$filename</a> <a href='#' id='close-tabs-$counter' class='close-tabs'>x</a></li>";
+            $content = file_get_contents($filetreepath . $filename);
+            
+            $divs .= "
 				<div id='tabs-$counter'>
 					<form action='render.php' target='rendered' method='post'>
 						<textarea name='code' id='tabs1-code' class='code-editor'>$content</textarea><br />
                         After Save: 
                         <select name='action'>
-                            <option>Run This File</option>
                             <option>Run Index File</option>
+                            <option>Run This File</option>
                             <option>Do Nothing</option>
                         </select>
                         <input type='hidden' name='file' value='$filename' />
@@ -28,11 +29,11 @@ class tabs {
 					</form>
 				</div>
 			";
-		}
-		$links .= "</ul>";
+        }
+        $links .= "</ul>";
 
-		return $links . $divs;
-	}
+        return $links . $divs;
+    }
 
 }
 
