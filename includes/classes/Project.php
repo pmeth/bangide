206de05da307658ab1c19e76f921d883a7d34773
petermeth <petermeth@192.168.0.190>
2012-04-22 15:10:59 -0400

    registration now reads skeleton files from the project_skeleton directory

--- includes/classes/Project.php
+++ includes/classes/Project.php
@@ -7,120 +7,102 @@
  */
 class Project {
 
-	protected $_db;
-	protected $_projecttable;
-	protected $_projectdirectory;
+    protected $_db;
+    protected $_projecttable;
+    protected $_projectdirectory;
 
-	function __construct(PDO $db, $projecttable = 'project', $projectdirectory = 'projects') {
-		$this->_db = $db;
-		$this->_projecttable = $projecttable;
-		$this->_projectdirectory = $projectdirectory;
-	}
+    function __construct(PDO $db, $projecttable = 'project', $projectdirectory = 'projects') {
+        $this->_db = $db;
+        $this->_projecttable = $projecttable;
+        $this->_projectdirectory = $projectdirectory;
+    }
 
-	public function generateNew($user_id, $db_user, $db_pass) {
-		$project = $this->getRandomDirectoryName();
+    public function generateNew($user_id, $db_user, $db_pass) {
+        $project = $this->getRandomDirectoryName();
 
-		$this->_generateFiles($project, $project, $db_user, $db_pass);
-		$this->_generateDatabase($project, $db_user, $db_pass);
-		$stmt = $this->_db->prepare('INSERT INTO ' . $this->_projecttable . ' SET user_id = :user_id, db_name = :db_name, db_user = :db_user, db_pass = :db_pass');
-		$stmt->execute(array(
-			 ':db_name' => $project,
-			 ':db_user' => $db_user,
-			 ':db_pass' => $db_pass,
-			 ':user_id' => $user_id,
-		));
-		return $project;
-	}
+        $this->_generateFiles($project, $project, $db_user, $db_pass);
+        $this->_generateDatabase($project, $db_user, $db_pass);
+        $stmt = $this->_db->prepare('INSERT INTO ' . $this->_projecttable . ' SET user_id = :user_id, db_name = :db_name, db_user = :db_user, db_pass = :db_pass');
+        $stmt->execute(array(
+            ':db_name' => $project,
+            ':db_user' => $db_user,
+            ':db_pass' => $db_pass,
+            ':user_id' => $user_id,
+        ));
+        return $project;
+    }
 
-	public function generateRandomAlpha($length) {
-		$string = "";
+    public function generateRandomAlpha($length) {
+        $string = "";
 
-		for ($i = 0; $i < $length; $i++) {
-			// random lowercase alphabet character
-			$string .= chr(rand(97, 122));
-		}
-		return $string;
-	}
+        for ($i = 0; $i < $length; $i++) {
+            // random lowercase alphabet character
+            $string .= chr(rand(97, 122));
+        }
+        return $string;
+    }
 
-	public function getRandomDirectoryName() {
-		$badcharacters = str_split('/?*:;{}]+.[^/?*:;{}\\');
-		$newname = uniqid("", true);
-		$newname = str_replace($badcharacters, '', $newname);
+    public function getRandomDirectoryName() {
+        $badcharacters = str_split('/?*:;{}]+.[^/?*:;{}\\');
+        $newname = uniqid("", true);
+        $newname = str_replace($badcharacters, '', $newname);
 
-		if (file_exists($this->_projectdirectory . DIRECTORY_SEPARATOR . $newname)) {
-			return $this->getRandomDirectoryName();
-		} else {
-			return $newname;
-		}
-	}
+        if (file_exists($this->_projectdirectory . DIRECTORY_SEPARATOR . $newname)) {
+            return $this->getRandomDirectoryName();
+        } else {
+            return $newname;
+        }
+    }
 
-	public function getProjectFromUserId($user_id) {
-		$stmt = $this->_db->prepare('SELECT db_name, db_user, db_pass FROM ' . $this->_projecttable . ' WHERE user_id=:user_id');
-		$stmt->execute(array(':user_id' => $user_id));
-		return $stmt->fetchObject();
-	}
+    public function getProjectFromUserId($user_id) {
+        $stmt = $this->_db->prepare('SELECT db_name, db_user, db_pass FROM ' . $this->_projecttable . ' WHERE user_id=:user_id');
+        $stmt->execute(array(':user_id' => $user_id));
+        return $stmt->fetchObject();
+    }
 
-	protected function _generateFiles($project, $db_name = 'DATABASE', $db_user = 'USERNAME', $db_pass = 'PASSWORD') {
-		// make the directories in order such that top level folders get created first
-		$folders = array(
-			 'includes',
-			 'images',
-			 'js',
-			 'css',
-		);
+    protected function _generateFiles($project, $db_name = 'DATABASE', $db_user = 'USERNAME', $db_pass = 'PASSWORD') {
+        $this->_recursiveCopy('project_skeleton', $this->_projectdirectory, $project);
+        $contents = "<?php
+\$db_host = 'localhost';
+\$db_name = '$db_name';
+\$db_user = '$db_user';
+\$db_pass = '$db_pass';
+?>";
+        $projectbase = $this->_projectdirectory . DIRECTORY_SEPARATOR . $project . DIRECTORY_SEPARATOR;
+        for ($i = 2; $i <= 15; $i++) {
+            $filetowrite = $projectbase . "exercise$i" . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR . 'db.php';
+            if (file_exists($filetowrite)) {
+                file_put_contents($filetowrite, $contents);
+            }
+        }
+    }
 
-		$files = array(
-			 array(
-				  'name' => 'index.php',
-				  'contents' => "<?php\n?>"
-			 ),
-			 array(
-				  'name' => 'includes' . DIRECTORY_SEPARATOR . 'db.php',
-				  'contents' => "<?php\n\n\$db_host = 'localhost';\n\$db_name = '$db_name';\n\$db_user = '$db_user';\n\$db_pass = '$db_pass';\n?>"
-			 ),
-			 array(
-				  'name' => 'includes' . DIRECTORY_SEPARATOR . 'header.php',
-				  'contents' => ''
-			 ),
-			 array(
-				  'name' => 'includes' . DIRECTORY_SEPARATOR . 'footer.php',
-				  'contents' => ''
-			 ),
-			 array(
-				  'name' => 'js' . DIRECTORY_SEPARATOR . 'common.js',
-				  'contents' => ""
-			 ),
-			 array(
-				  'name' => 'images' . DIRECTORY_SEPARATOR . 'logo.gif',
-				  'contents' => file_get_contents('http://tug.ca/Graphics/TUG_Live-Logo_150x95.gif')
-			 ),
-			 array(
-				  'name' => 'css' . DIRECTORY_SEPARATOR . 'common.css',
-				  'contents' => ""
-			 ),
-		);
-
-		$basedir = $this->_projectdirectory . DIRECTORY_SEPARATOR . $project;
-		mkdir($basedir);
-		$basedir .= DIRECTORY_SEPARATOR;
-
-		foreach ($folders as $folder) {
-			mkdir($basedir . $folder);
-		}
-
-		foreach ($files as $file) {
-			file_put_contents($basedir . $file['name'], $file['contents']);
-		}
-	}
-
-	protected function _generateDatabase($project, $username, $password) {
+    protected function _generateDatabase($project, $username, $password) {
 
 
-		$this->_db->exec("CREATE DATABASE `$project`");
-		$this->_db->exec("CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'");
-		$this->_db->exec("GRANT ALL PRIVILEGES ON `$project`.* TO '$username'@'localhost'");
+        $this->_db->exec("CREATE DATABASE `$project`");
+        $this->_db->exec("CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'");
+        $this->_db->exec("GRANT ALL PRIVILEGES ON `$project`.* TO '$username'@'localhost'");
+    }
 
-	}
+    protected function _recursiveCopy($source, $dest, $diffDir = '') {
+        $sourceHandle = opendir($source);
+        if (!$diffDir)
+            $diffDir = $source;
+
+        mkdir($dest . '/' . $diffDir);
+
+        while ($res = readdir($sourceHandle)) {
+            if ($res == '.' || $res == '..')
+                continue;
+
+            if (is_dir($source . '/' . $res)) {
+                $this->_recursiveCopy($source . '/' . $res, $dest, $diffDir . '/' . $res);
+            } else {
+                copy($source . '/' . $res, $dest . '/' . $diffDir . '/' . $res);
+            }
+        }
+    }
 
 }
 
