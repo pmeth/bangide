15a3e14a0f1db2a65df1549c64c47c9a8f4a3302
Peter Meth <pmeth@rogers.com>
2012-04-22 23:19:49 -0400

    putting up instructions for lessons 2 and 3.

--- project_skeleton/exercise2/index.php
+++ project_skeleton/exercise2/index.php
@@ -1,3 +1,83 @@
 <?php
+/*-------------------------------------------------------
+1. create an html page with 4 divs
+DIV 1:
+displays the TUG logo
 
+DIV 2:
+displays a menu with 3 links separated by double colon (::)
+HOME (index.php) :: PRODUCTS (products.php) :: CONTACT US (contactus.php)
+
+DIV 3:
+displays a heading saying HOME, then a welcome message
+
+DIV 4:
+displays a copyright followed by the current year followed by a company name (TUG maybe?)
+hint: the html code &copy; will display a copyright symbol
+hint: the php function date('Y') will give the current year
+
+save and run to make sure it looks right
+
+---------------------------------------------------------
+2. copy the start of your html from index.php along with the first 2 divs.
+open header.php and paste what you just copied
+
+save and run the header file to see what the header will look like
+(note this file is not a full html document, because there is no </body> or </html>
+but it should still show up fine in the browser
+
+---------------------------------------------------------
+3. copy the last div and the closing html tag.
+open footer.php and paste what you just copied
+
+save and run the footer file to see what the footer will look like
+(note this file is not a full html document, because it does not contain the starting
+elements of an <html> document but it should still show up fine in the browser
+
+---------------------------------------------------------
+4. go back to your index.php and remove everything except the 3rd div (the one with the heading
+and welcome message).
+
+save and run to see what the new homepage looks like
+
+---------------------------------------------------------
+5. add a php tag above your div that includes the header.php file, and one below that includes
+the footer.php file.
+syntax: 
+	include "<filename>"
+
+save and run to once again see the completed homepage
+
+---------------------------------------------------------
+6. open products.php and put a single div that displays a heading saying PRODUCTS followed by
+a list of some products
+
+add the same php sections to include header and footer
+
+save and run
+
+---------------------------------------------------------
+7. open contactus.php and put a single div that displays a heading saying CONTACT US followed by
+some contact info
+
+add the same php sections to include header and footer
+
+save and run
+
+---------------------------------------------------------
+8. (optional) open css/stylesheet.css and make some changes to dress your page to your liking.
+
+---------------------------------------------------------*/
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Page</title>
+		<link rel="stylesheet" href="css/stylesheet.css">
+	</head>
+	<body>
+
+		<!--START YOUR WORK HERE -->
+
+	</body>
+</html>
--- project_skeleton/exercise3/index.php
+++ project_skeleton/exercise3/index.php
@@ -1,3 +1,36 @@
 <?php
+/*-------------------------------------------------------
+1. create a basic html form with the following fields
+- name (text)
+- email address (text)
+- message (multiline textarea)
+- submit button
+
+make the action of the form to "handler.php" and set the method to "post"
+
+save and view
+
+---------------------------------------------------------
+2. open handler.php and do a print_r on the $_POST variable
+
+save and run the index file.
+submit a form and see what happens
+
+---------------------------------------------------------
+3. modify your handler to check that name, message are not blank.
+also check that email address is valid
+if not, print a message with a link to go back to the index
+hint: to check if a string is blank, write an if statement with == "" 
+hint: use an if with filter_var($string, FILTER_VALIDATE_EMAIL) to check for valid emails
+hint: use exit to stop the php script from going any further
+
+---------------------------------------------------------
+4. after the checks in step 3, your handler will send an email to you with all the fields of the form.
+it will print a message thanking the user for their submission.
+note: you can send an email to my address if you prefer not to put your own email
+
+---------------------------------------------------------*/
+
+// WRITE YOUR CODE HERE
 
 ?>
