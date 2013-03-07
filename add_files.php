501eea54cd49fbf56839cb343a030c2a54202a4e
Peter Meth <pmeth@centah.com>
2013-03-07 17:29:35 -0500

    adding error handling around creating files

--- add_files.php
+++ add_files.php
@@ -5,10 +5,18 @@ $loggedinuser = $session['user']['username'];
 $dbuser = $session['db_user'];
 
 if (!empty($_POST['fileset']) && $_POST['fileset'] == 'index') {
-	$project->generateIndexFile($session['user']['projectfolder']);
+	try {
+		$project->generateIndexFile($session['user']['projectfolder']);
+	} catch (Exception $e) {
+		die('Unable to create files.  Perhaps you did not create the project directory with write permission');
+	}
 }
 
 if (!empty($_POST['fileset']) && $_POST['fileset'] != 'index') {
-	$project->generateFiles($session['user']['projectfolder'], $_POST['fileset']);
+	try {
+		$project->generateFiles($session['user']['projectfolder'], $_POST['fileset']);
+	} catch (Exception $e) {
+		die('Unable to create files.  Perhaps you did not create the project directory with write permission');
+	}
 }
 require 'simple.php';
